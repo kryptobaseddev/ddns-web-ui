2f3a5b8c841f98a24fbaae3fbfee61cd2a2ba4da
KryptoBasedDev <95310582+kryptobaseddev@users.noreply.github.com>
2024-09-27 00:12:53 +0000

    update gunicorn process to start scheduler

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
-# wsgi.py
 from app import create_app
+from app.utils.scheduler import start_scheduler
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run()
+if __name__ != "__main__":
+    with app.app_context():
+        start_scheduler(app)  # Start the scheduler in the master process
